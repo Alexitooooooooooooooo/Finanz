08d668c44084c2633f56f320a9b45e426e9d94cc
Alexitooooooooooooooo <javieralexfeik1@gmail.com>
2025-11-26 16:25:44 -0300

    fix: fixes on users

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\userController;
+use App\Http\Controllers\ClientsController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/users', [UserController::class, 'index']);
