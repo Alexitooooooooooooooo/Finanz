ad5f6ed8b5f99affebca5b17e9000bafcb3c408a
Alexitooooooooooooooo <javieralexfeik1@gmail.com>
2025-11-26 16:20:05 -0300

    feat: install laravel sanctum

--- app/Models/Clients.php
+++ app/Models/Clients.php
@@ -7,5 +7,17 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Clients extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'name',
+        'email',
+        'balance',
+    ];
+
+    public function transactions()
+    {
+        return $this->belongsToMany(Transaction::class, 'client_transaction', 'client_id', 'transaction_id');
+    }
 }
