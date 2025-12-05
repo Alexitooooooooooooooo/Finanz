cedb14cf870fedab76ab56c902339fccea420023
Alexitooooooooooooooo <Javieralexfeik1@gmail.com>
2025-12-04 22:42:02 -0400

    fix: fix login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -16,18 +16,38 @@ class AuthController extends Controller{
 
     public function me(Request $request): JsonResponse
     {
-        return response()->json(['message' => 'User authenticated.', 'data' => $request->user()], 200);
-    }
+            $user = $request->user();
+            return response()->json([
+                'success' => true,
+                'user' => [
+                    'id' => $user->id,
+                    'email' => $user->email,
+                    'created_at' => $user->created_at,
+                    'updated_at' => $user->updated_at,
+
+                ]
+            ], 200);    }
 
     public function login(AuthRequest $request): JsonResponse
     {
-        $credentials = $request->validated();
 
         try {
-        $authdata = $this->authRepository->login($credentials);
-            return response()->json(['message' => 'User logged in successfully.', 'data' => $authdata], 200);
+            $credentials = $request->validated();
+            $authData = $this->authRepository->login($credentials);
+
+            return response()->json([
+                'success' => true,
+                'message' => 'Successful login',
+                    'user' => $authData['user'],
+                    'token' => $authData['token']
+            ], 200);
+
         } catch (ValidationException $e) {
-            return response()->json(['errors' => $e->errors()], 422);
+            return response()->json([
+                'success' => false,
+                'message' => 'Authentication error',
+                'errors' => $e->errors()
+            ], 402);
         }
     }
 
